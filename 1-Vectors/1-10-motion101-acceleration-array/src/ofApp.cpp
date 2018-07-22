991d816fef7c1e49e0153debd991140465a2b9b1
Guido Schmidt <git@guidoschmidt.cc>
2018-07-22 20:51:19 +0200

    ADD/1-Vectors/1-10-motion101-acceleration-array: array of movers example

---
+++ 1-Vectors/1-10-motion101-acceleration-array/src/ofApp.cpp
@@ -0,0 +1,78 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+  for (unsigned int i = 0; i < 20; i++) {
+    this->_movers[i] = new Mover();
+  }
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+  for (Mover* mover : this->_movers) {
+    mover->update();
+  }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+  for (Mover* mover : this->_movers) {
+    mover->display();
+  }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
