2b310cbdce66c0714ea8cd3aa2bbfe34fd331c62
Guido Schmidt <guido.schmidt@immundiagnostik.com>
2018-07-26 13:12:09 +0200

    ADD/1-Vectors/1-03-vector-subtraction

---
+++ 1-Vectors/1-03-vector-subtraction/src/ofApp.cpp
@@ -0,0 +1,77 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+  glm::vec2 mouse = glm::vec2(ofGetMouseX(), ofGetMouseY());
+  glm::vec2 center = glm::vec2(ofGetWidth() / 2, ofGetHeight() / 2);
+  glm::vec2 pos = mouse - center;
+  ofSetLineWidth(2.0);
+  ofSetColor(212, 42, 200);
+  ofTranslate(center);
+  ofDrawLine(0, 0, pos.x, pos.y);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
