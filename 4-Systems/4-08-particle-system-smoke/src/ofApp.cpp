107ff970f379732de5175b2188d3e7394cc48bf9
Guido Schmidt <git@guidoschmidt.cc>
2019-07-25 20:22:49 +0200

    ADD/4-Systems/4-08-particle-system-smoke

---
+++ 4-Systems/4-08-particle-system-smoke/src/ofApp.cpp
@@ -0,0 +1,76 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+  glm::vec2 systemOrigin = { ofGetWidth() / 2, ofGetHeight() / 2 };
+  _particleSystem = new ParticleSystem(systemOrigin, "texture.png");
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+  ofBackground(0);
+  _particleSystem->addParticle();
+  glm::vec2 gravity = glm::vec2(0, -0.01);
+  _particleSystem->applyForce(gravity);
+  _particleSystem->run();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
